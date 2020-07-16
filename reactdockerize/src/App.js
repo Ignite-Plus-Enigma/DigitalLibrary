42f8c21f2f98245141a5991b7fd4cec092cbd720
Ankitha Chowlur <54694545+ankithac@users.noreply.github.com>
2020-07-16 16:14:25 +0530

    Update App.js

--- reactdockerize/src/App.js
+++ reactdockerize/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AudioFile from './Components/AudioFile'
+import  Search  from './Components/Search'
 
 function App() {
   return (
     <div>
       {/* <header className="App-header"> */}
-        <AudioFile/>
+       <Search/>
       {/* </header> */}
     </div>
   );
