c3d29ac6a27d8c510d4cb9bac9fe1603b188043c
Prakruthi <prakruthim25@gmail.com>
2020-07-18 15:41:08 +0530

    Footer Added

--- reactdockerize/src/App.js
+++ reactdockerize/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AudioFile from './Components/AudioFile'
+import './Components/Footer.css';
 
 function App() {
   return (
